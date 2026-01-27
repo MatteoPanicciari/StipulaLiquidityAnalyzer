fd53efb9f69ec6116589a8a6eaaa118173483669
MatteoPanicciari <matteo.panicciari@gmail.com>
2026-01-27 13:42:48 +0100

    execution from command line (click)

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import sys
 import antlr4
+import click
 
 from generated.StipulaLexer import StipulaLexer
 from generated.StipulaParser import StipulaParser
-
 from classes.liquidity_visitor import LiquidityVisitor
 
-def _main(file_path: str, is_verbose: bool=False):
+def run(file_path: str, is_verbose: bool = False):
     input_stream = antlr4.FileStream(file_path)
     lexer = StipulaLexer(input_stream)
     stream = antlr4.CommonTokenStream(lexer)
@@ -18,18 +18,17 @@ def _main(file_path: str, is_verbose: bool=False):
         sys.exit(1)
     LiquidityVisitor(is_verbose).visit(tree)
 
-if __name__ == '__main__':
-    #_main('./TESTS/Ugly.stipula')
-    _main('./TESTS/Fill_Move.stipula')
-    _main('./TESTS/Non_Liquid_Fill_Move.stipula')
-    #_main('TESTS/AbsCompTest.stipula')
-    #_main('./TESTS/Ping_Pong.stipula')
-    #_main('./TESTS/Bet.stipula')
-    #_main('./TESTS/Linear_Automaton.stipula')
+@click.command()
+@click.argument("file_path", default="./TESTS/Fill_Move.stipula")
+@click.option("-v", "--verbose", default=False)
+def cli_main(file_path, verbose):
+    run(file_path, verbose)
+
+if __name__ == "__main__":
+    cli_main()
 
 # TODO
 #   commentare
-#   esecuzione da riga di comando con 'click'
 #   togliere(?) i visitExpression dato che non devo assicurarmi dei tipi o degli id
 #   capire se va bene usare il complete algorithm se anche una sola abs_comp in un contratto ha i tipi di asset che non sono singoletti
 #   mettere il mio codice nel progetto di erik
