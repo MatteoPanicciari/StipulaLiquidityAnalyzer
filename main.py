161df11b21ab71251141a2ab8a61f7176da85950
MatteoPanicciari <matteo.panicciari@gmail.com>
2026-03-04 14:28:30 +0100

    edited output messages (asset types)

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ def run(file_path: str, function_frequency: int, is_verbose: bool = False):
 @click.command()
 @click.argument("file_path", default="./TESTS")
 @click.argument("function_frequency", default=3)
-@click.option("-v", "--verbose", "is_verbose", default=True, show_default=True, is_flag=True, help='Show verbose output.')
+@click.option("-v", "--verbose", "is_verbose", default=False, show_default=True, is_flag=True, help='Show verbose output.')
 def cli_main(file_path, function_frequency, is_verbose):
     path = Path(file_path)
 
